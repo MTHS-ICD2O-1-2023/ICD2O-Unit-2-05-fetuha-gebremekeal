edd9dda72f5ad747988f5b957b14efba7da4fb3c
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-03-25 18:31:29 +0000

    text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+// Created by: Fetuha
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function findIncomeClicked() {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('hourly-income').value)
+  const govTax = (baseOfTriangle * heightOfTriangle) * 0.5
+  const totalEarnings = (baseOfTriangle * heightOfTriangle) * 0.5
+  document.getElementById("area").innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+}
